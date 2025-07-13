277fa34d5349b1f8fcacf101051527de8bd07788
Daniela Brunetto <dbrunetto.informatica@centralshop.com.py>
2025-07-13 13:45:47 -0300

    cambios en los colores principales, iconos y estilos del home

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+        <div className='main-banner'>
+            <img src="./img/banner/main.png" alt="banner principal" />
+        </div>
+    </section>
+  )
+}
+
+export default Hero
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from '../../components/header/Header'
+import Hero from '../../components/hero/Hero'
 
 const Home = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Hero />
+    </>
   )
 }
 
